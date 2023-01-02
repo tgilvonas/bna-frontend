181cbfb8ab0f7f6a1266d8039bf69283587d104b
Tomas Gilvonauskas <tgilvonas@gmail.com>
2023-01-02 20:18:34 +0200

    Mobile menu was installed

--- assets-src/frontend-main.js
+++ assets-src/frontend-main.js
@@ -1,1 +1,16 @@
+import MobileTouchMenu from 'mobile-touch-menu';
+import $ from 'jquery'
 
+$(document).ready(function(){
+    let mobileTouchMenu = new MobileTouchMenu({
+        width: '300px',
+        direction: 'left',
+        swipeDistance: 120
+    });
+    $('#open-mobile-menu').click(function(){
+        mobileTouchMenu.show();
+    });
+    $('#close-mobile-menu').click(function(){
+        mobileTouchMenu.hide();
+    });
+});
